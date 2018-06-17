0f69d03ac93509d741bdf736675663e5a7e828ed
sukapenpen <ne280194@senshu-u.jp>
2018-06-17 22:07:44 +0900

    Object Pool is little available.

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -7,23 +7,51 @@ public class EnemyManager : MonoBehaviour {
   public int len;
   public GameObject enemy_pre;
   private List<GameObject> enemys = new List<GameObject> ();
+  private int count = 0;
 
   public bool test_flg = false; //作られてることが目に見えるようにするためのテスト用flg
 
   void Start () {
-    GameObject enemy;
     for (int i = 0; i < len; i++) {
-      enemy = (GameObject)Instantiate (enemy_pre, new Vector3 (this.transform.position.x, -i, this.transform.position.z), Quaternion.identity);
-      enemy.transform.localScale = new Vector3 (30, 30, 30);
-      enemy.gameObject.SetActive (false);
-      enemy.transform.parent = this.transform;
-      enemys.Add (enemy);
+      CreateEnemy (false, -i);
     }
   }
 
   void Update () {
-    foreach (GameObject obj in enemys) {
-      obj.gameObject.SetActive (test_flg);
+    if (Input.GetKey (KeyCode.T)) {
+      count++;
+      GameObject ene;
+      ene = GetEnemy ();
+      Debug.Log(enemys.Count);
+    }
+    if (Input.GetKey (KeyCode.F)) {
+      foreach (GameObject obj in enemys) {
+        if (obj.activeSelf) {
+          obj.SetActive (false);
+          break;
+        }
+      }
     }
   }
+
+  GameObject GetEnemy () {
+    foreach (GameObject obj in enemys) {
+      if (!obj.activeSelf) {
+        obj.SetActive(true);
+        return obj;
+      }
+    }
+    CreateEnemy (true, 1 * count);
+    return enemys [enemys.Count - 1];
+  }
+
+  void CreateEnemy (bool flg, int y) {
+    GameObject enemy;
+    enemy = (GameObject)Instantiate (enemy_pre, new Vector3 (this.transform.position.x, y, this.transform.position.z), Quaternion.identity);
+    enemy.transform.localScale = new Vector3 (30, 30, 30);
+    enemy.gameObject.SetActive (flg);
+    enemy.transform.parent = this.transform;
+    enemys.Add (enemy);
+  }
+
 }
